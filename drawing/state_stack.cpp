5f27c87e317f53e07c0225f35688f3da40a8a5e7
Андрей <grigormaklaud@gmail.com>
2024-06-13 21:10:07 +0300

    change state stack

--- drawing/state_stack.cpp
+++ drawing/state_stack.cpp
@@ -31,6 +31,18 @@ void state_stack::handle_event(const sf::Event& event) {
   apply_pending_changes();
 }
 
+void state_stack::push_state(state_id state_id) {
+  pending_list_.emplace_back(state_action::push, state_id);
+}
+
+void state_stack::pop_state() {
+  pending_list_.emplace_back(state_action::pop);
+}
+
+void state_stack::clear_states() {
+  pending_list_.emplace_back(state_action::clear);
+}
+
 bool state_stack::empty() const noexcept {
   return stack_.empty();
 }
@@ -42,6 +54,21 @@ state::state_ptr state_stack::create_state(state_id id) {
   assert(found != factories_.end());
 
   return found->second();
+}
+
+void state_stack::apply_pending_changes() {
+  for (auto pending_change : pending_list_) {
+    switch (pending_change.action) {
+      case state_action::push:
+        stack_.push_back(create_state(pending_change.state_id));
+        break;
+      case state_action::pop:
+        stack_.pop_back();
+      case state_action::clear:
+        stack_.clear();
+        break;
+    }
+  }
 }
 
 }  // namespace drawing
